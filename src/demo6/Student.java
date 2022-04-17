8bdfdc73ab8b97cbb43496f660cf9010a6c53a4f
PawanMagic <pawan.kumar2magicedtech.com>
2022-04-17 16:37:38 +0530

    chnges

--- src/demo6/Student.java
+++ src/demo6/Student.java
@@ -7,6 +7,8 @@ public static void main(String[] args) {
 	Student s= new Student();
 	System.out.println(s.rollno);
 	System.out.println(s.sname);
+	System.out.println(s.sname);
+	System.out.println(s.sname);
 	
 	
 }
