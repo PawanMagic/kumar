d4b1a69aee404ddc95989a2c297304f43b0520e2
PawanMagic <pawan.kumar2magicedtech.com>
2022-04-17 16:28:26 +0530

    commit in kumar

---
+++ src/demo6/Student.java
@@ -0,0 +1,13 @@
+package demo6;
+
+public class Student {
+	int rollno=10;
+	String sname="pawan";
+public static void main(String[] args) {
+	Student s= new Student();
+	System.out.println(s.rollno);
+	System.out.println(s.sname);
+	
+	
+}
+}
